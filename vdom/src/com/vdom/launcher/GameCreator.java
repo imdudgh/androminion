8f058ac355ec7dfe79001862ff6e8893b552faf6
imdudgh <imdudgh@snu.ac.kr>
2019-02-06 03:22:58 +0900

    Working on menu 1...

--- vdom/src/com/vdom/launcher/GameCreator.java
+++ vdom/src/com/vdom/launcher/GameCreator.java
@@ -1,5 +1,9 @@
 package com.vdom.launcher;
 
+import com.vdom.api.Card;
+import com.vdom.api.GameType;
+import com.vdom.core.CardSet;
+import com.vdom.core.Cards;
 import com.vdom.core.Expansion;
 import java.util.*;
 import java.io.*;
@@ -8,73 +12,80 @@ public class GameCreator
 {
     public static String[] getGameArgs()
     {
+        /*
+         * Use File I/O to keep previously played card sets
+         * The files will be placed in the topmost directory(androminion/vdom/filename)
+         */
+
         List<String> args = new ArrayList<String>();
+        GameType type = null;
 
-        Scanner in = new Scanner(System.in);
-
-        // First specify which kingdom cards to use
         System.out.println("Dominion Game Launcher");
         System.out.println("========================");
-        System.out.println("<1> Set up a new game");
-        System.out.println("<2> Play preset");
-        System.out.println("<3> View previously played games");
-        System.out.println("<4> Replay saved games");
-
-        int selection = -1;
-        do
+        do  // First retrieve game type. Do not forget to add "Specified" for manual card selection.
         {
-            try
-            {
-                selection = System.in.read();
-            }
-            catch (IOException ex)
-            {
+            System.out.println("<1> Set up a new game");
+            System.out.println("<2> Play preset");
+            System.out.println("<3> View previously played games");
+            System.out.println("<4> Replay saved games");
 
-            }
-        }while(selection < (int)'1' || selection > (int)'4');
+            int selection = -1;
+            do
+            {
+                try
+                {
+                    selection = System.in.read();
+                }
+                catch (IOException ex)
+                {
+
+                }
+            } while (selection < (int) '1' || selection > (int) '4');
 
         /*
         System.out.println();
         System.out.println();
         */
 
-        switch (selection - (int)'0')
-        {
-            case 1:
-
-            case 2:
-                // First make the user select an expansion
-                System.out.println("Preset Selection. Enter Expansion:");
-                Expansion expansion = null;
-                do
-                {
-                    try
+            switch (selection - (int) '0')
+            {
+                case 1:
+                    new Cards();    // this is necessary, for the static initializer will not add cards to kingdom enums until an instance is created.
+                    for(Card card : Expansion.Empires.getKingdomCards())
                     {
-                        String str = in.nextLine();
-                        expansion = Expansion.valueOf(str);
+                        System.out.print(card);
                     }
-                    catch (Exception ex)
+                case 2:
+                    // First make the user select an expansion
+                    System.out.println("Preset Selection. Enter Expansion:");
+                    Expansion expansion = (Expansion) selectOne(Expansion.values());
+
+                    List<GameType> presets = new ArrayList<GameType>();
+                    for (GameType t : GameType.values())
                     {
-                        if(ex instanceof IllegalArgumentException)
-                        {
-                            System.out.print("Name incorrect. Expansions:");
-                            for(Expansion exp: Expansion.values())
-                            {
-                                System.out.print(" " + exp);
-                            }
-                            System.out.println();
-                        }
+                        List<Expansion> exps = t.getExpansions();
+                        if (exps.size() > 0 && exps.get(0) == expansion)
+                            presets.add(t);
                     }
-                }while(expansion == null);
 
+                    type = (GameType)selectOne(presets.toArray());
+                    break;
+                case 3:
+                    try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                            new FileOutputStream("filename.txt"), "utf-8"))) {
+                        writer.write("something");
+                    }
+                    catch (IOException ex)
+                    {
 
-
-                break;
-            case 3:
-            case 4:
-        }
+                    }
+                case 4:
+            }
+        }while(type == null);
+        args.add("-type" + type.toString());
 
         // TODO fetch arguments here
+
 
         return args.toArray(new String[args.size()]);
     }
@@ -83,5 +94,63 @@ public class GameCreator
     {
         // TODO
         return null;
+    }
+
+    private static Object selectOne(Object[] list)
+    {
+        Scanner in = new Scanner(System.in);
+
+        for(int i = 0; i < list.length; i++)
+        {
+            System.out.println("<" + (i + 1) +"> " + objectInfo(list[i]));
+        }
+
+        int selection = 0;
+        do
+        {
+            try
+            {
+                selection = Integer.parseInt(in.nextLine());
+            }
+            catch(NumberFormatException ex) {}
+        }while(selection <= 0 || selection > list.length);
+
+        return list[selection - 1];
+    }
+
+    private static String objectInfo(Object object)
+    {
+        if(object instanceof GameType)
+        {
+            GameType type = (GameType)object;
+            StringBuilder str = new StringBuilder();
+            str.append(type.getName());
+            str.append("(");
+            boolean first = true;
+            for (Expansion expansion : type.getExpansions())
+            {
+                if(first)
+                   first = false;
+                else
+                    str.append(",");
+                str.append(expansion.toString());
+            }
+            str.append("): ");
+
+            first = true;
+            List<Card> cards = CardSet.getCardSetMap().get(type).getCards();
+            for(Card card : cards)
+            {
+                if(first)
+                    first = false;
+                else
+                    str.append(",");
+                str.append(card.getName());
+            }
+
+            return str.toString();
+        }
+
+        return object.toString();
     }
 }
